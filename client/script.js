0abd48688893413aea927f22ca2f4404b717d617
case-2000 <h23kajja@du.se>
2025-01-15 15:16:17 +0100

    sista tilläggen och ändringar

--- client/script.js
+++ client/script.js
@@ -3,16 +3,16 @@ const url = "http://localhost:3000/users";
 fetch(url)
   .then((data) => data.json())
   .then((users) => {
-    let html = `<ul class="list-group userListContainer">`;
+    console.log(users);
+    let html = `<h1>Users</h1><ul class="list-group userListContainer"> `;
     users.forEach((user) => {
-      console.log(users);
       const backgroundColor = user.color || "#f55555";
       html += `
-      <li class="mt-5 listItem " style="background-color: ${backgroundColor};"> 
-        <p>id: ${user.id}</p>
-        <p>firstname: ${user.firstName}</p>
-        <p>lastname: ${user.lastName}</p>
-        <p>username: ${user.username}</p>
+      <li class="listItem " style="background-color: ${backgroundColor};"> 
+        <p>Id: ${user.id}</p>
+        <p>Firstname: ${user.firstName}</p>
+        <p>Lastname: ${user.lastName}</p>
+        <p>Username: ${user.username}</p>
       </li>`;
     });
     html += `</ul>`;
